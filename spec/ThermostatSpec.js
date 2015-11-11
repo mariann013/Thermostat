f48120a855cff3b00f84630407ad27716f09b4bd
Mari-Ann Meling <mariann013@gmail.com>
2015-11-11 16:35:31 +0000

    temp max

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,6 +4,8 @@ describe("Thermostat", function() {
   var thermostat;
   var powerSavingMode;
   var minimumTemperature;
+  var maximumTemperature;
+  var poweSavingModeTemperature;
 
 
   beforeEach( function() {
@@ -33,12 +35,23 @@ describe("Thermostat", function() {
   it("should have power saving mode on", function() {
     thermostat.powerSavingOn();
     expect(thermostat.powerSavingMode).toBe(true);
-  })
+  });
 
   it("should have power saving mode off", function() {
     thermostat.powerSavingOff();
     expect(thermostat.powerSavingMode).toBe(false);
-  })
+  });
+
+  it("should set maximum temp of 25 degrees if PSM is on", function () {
+    thermostat.temperature = 25;
+    expect(function(){ thermostat.increase(); }).toThrow("Cannot increase temperature powersave mode on");
+  });
+
+  it("should set maximum temp of 32 degrees if PSM is off", function () {
+    thermostat.powerSavingOff();
+    thermostat.temperature = 32;
+    expect(function(){ thermostat.increase(); }).toThrow("Cannot increase temperature beyond maximum");
+  });
 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,10 +3,18 @@ function Thermostat() {
   this.temperature = 20;
   this.powerSavingMode = true;
   this.minimumTemperature = 10;
+  this.maximumTemperature = 32;
+  this.poweSavingModeTemperature = 25;
 };
 
   Thermostat.prototype.increase = function () {
+    if (this.powerSavingMode === true && this.temperature === this.poweSavingModeTemperature) {
+      throw "Cannot increase temperature powersave mode on";
+    } else if (this.powerSavingMode === false && this.temperature === this.maximumTemperature){
+      throw "Cannot increase temperature beyond maximum";
+    } else{
     this.temperature += 1;
+    }
   };
 
   Thermostat.prototype.decrease = function () {
