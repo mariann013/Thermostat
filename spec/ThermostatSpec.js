3a3c2d10a291dd6ad843456ab8b16d7f0d31234c
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-11-11 16:04:56 +0000

    minimum temperate added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,25 +2,31 @@ describe("Thermostat", function() {
 
   var temperature;
   var thermostat;
+  var minimumTemperature;
 
   beforeEach( function() {
     thermostat = new Thermostat();
-  })
+    minimumTemperature = thermostat.minimumTemperature;
+  });
 
 
   it("has a default temperature of 20 degrees", function() {
     expect(thermostat.temperature).toBe(20);
-  })
+  });
 
   it("can increase the temperature", function() {
     thermostat.increase();
     expect(thermostat.temperature).toBe(21);
-  })
+  });
 
   it("can decrease the temperature", function() {
     thermostat.decrease();
     expect(thermostat.temperature).toBe(19);
-  })
+  });
 
+  it("has a minimum temperate", function() {
+    thermostat.temperature = minimumTemperature;
+    expect(function(){ thermostat.decrease(); }).toThrow("Cannot decrease temperature beyond minimum");
+  });
 
-  })
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,17 @@
 function Thermostat() {
 
   this.temperature = 20;
+  this.minimumTemperature = 10;
+};
 
   Thermostat.prototype.increase = function () {
     this.temperature += 1;
   };
 
   Thermostat.prototype.decrease = function () {
-    this.temperature -= 1
+    if (this.temperature === this.minimumTemperature) {
+      throw "Cannot decrease temperature beyond minimum";
+    } else {
+      this.temperature -= 1;
+    }
   };
-}
