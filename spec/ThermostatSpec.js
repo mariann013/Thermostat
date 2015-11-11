f8eddc1e5d0ad4d1f46dd306168d104815ba06eb
Mari-Ann Meling <mariann013@gmail.com>
2015-11-11 15:04:46 +0000

    increase/decrease temperature by 1

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,6 +10,17 @@ describe("Thermostat", function() {
 
   it("has a default temperature of 20 degrees", function() {
     expect(thermostat.temperature).toBe(20);
-  });
+  })
+
+  it("can increase the temperature", function() {
+    thermostat.increase();
+    expect(thermostat.temperature).toBe(21);
+  })
+
+  it("can decrease the temperature", function() {
+    thermostat.decrease();
+    expect(thermostat.temperature).toBe(19);
+  })
+
 
   })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,12 @@
 function Thermostat() {
 
   this.temperature = 20;
+
+  Thermostat.prototype.increase = function () {
+    this.temperature += 1;
+  };
+
+  Thermostat.prototype.decrease = function () {
+    this.temperature -= 1
+  };
 }
