65d72c28ad3fb0aca279f3d79e7e0b3928317e47
Mari-Ann Meling <mariann013@gmail.com>
2015-11-11 16:21:16 +0000

    power saving function on

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,6 +2,7 @@ describe("Thermostat", function() {
 
   var temperature;
   var thermostat;
+  var powerSavingMode;
 
   beforeEach( function() {
     thermostat = new Thermostat();
@@ -22,5 +23,14 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toBe(19);
   })
 
+  it("should have power saving mode on", function() {
+    thermostat.powerSavingOn();
+    expect(thermostat.powerSavingMode).toBe(true);
+  })
+
+  it("should have power saving mode off", function() {
+    thermostat.powerSavingOff();
+    expect(thermostat.powerSavingMode).toBe(false);
+  })
 
   })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,21 @@
 function Thermostat() {
 
   this.temperature = 20;
+  this.powerSavingMode = true;
 
   Thermostat.prototype.increase = function () {
     this.temperature += 1;
   };
 
   Thermostat.prototype.decrease = function () {
-    this.temperature -= 1
+    this.temperature -= 1;
+  };
+
+  Thermostat.prototype.powerSavingOn = function () {
+    this.powerSavingMode = true;
+  };
+
+  Thermostat.prototype.powerSavingOff = function () {
+    this.powerSavingMode = false;
   };
 }
